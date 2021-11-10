9dc043e0ceb72aa37749b199a9d9282110c03997
Charles <charleschen0809@gmail.com>
2021-11-10 18:09:10 -0500

    Fixed Grade.java logic

--- src/grade/Grade.java
+++ src/grade/Grade.java
@@ -14,6 +14,7 @@ public class Grade {
         Scanner scan = new Scanner(System.in);
         System.out.println("This program will convert the letter grade into a numerical grade. Please enter a letter grade: ");
         String input = scan.nextLine();
+        input = input.toUpperCase();
         if (input.startsWith("A") || input.startsWith("B") || input.startsWith("C") || input.startsWith("D") || input.startsWith("F")) {
             getLetterGrade(input);
         } else {
@@ -23,23 +24,16 @@ public class Grade {
 
     public static double getLetterGrade(String grade) {
         Map<Character, Double> gradeToNumber =  setMap();
-        int numGrade = 4;
         if (grade.charAt(1) == '+') {
             if (grade.charAt(0) == 'A') {
                 return 4.0;
             }
-
-            if (gradeToNumber.containsKey(grade.charAt(0))) {
-                return gradeToNumber.get(grade.charAt(0))+ .3;
-            }
+            return gradeToNumber.get(grade.charAt(0))+ .3;
         } else if (grade.charAt(1) == '-') {
-
+            return gradeToNumber.get(grade.charAt(0))-.3;
         } else {
-            if (grade.charAt(0) == 'A') {
-                return 4.0;
-            }
+            return gradeToNumber.get(grade.charAt(0));
         }
-        return 5;
     }
 
     public static Map<Character, Double> setMap() {
